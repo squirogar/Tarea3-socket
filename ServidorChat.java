6737dd0531ff14b95e0c6ac12a2f436614263583
emoralesv <50589288+emoralesv@users.noreply.github.com>
2019-05-17 23:52:52 -0400

    Update ServidorChat.java

--- ServidorChat.java
+++ ServidorChat.java
@@ -7,10 +7,11 @@ public class ServidorChat {
     ServerSocket socketServidor = null;
     boolean listening = true;
     Socket socketCliente = null;
-    String entrada = "";
+	String entrada = "";
+		
     try {
     	socketServidor = new ServerSocket(4444);
-    	System.out.println("Se creó socket del servidor en el puerto 4444");
+    	System.out.println("Se creo socket del servidor en el puerto 4444");
     } catch (IOException e) {
         System.out.println("Error, el puerto 4444 está siendo ocupado por otra"
           + "aplicacion");
@@ -19,35 +20,44 @@ public class ServidorChat {
     }
 
     System.out.println("Servidor listo para aceptar requerimiento de "
-    	+ "clientes...");
-    
+		+ "clientes...");
+		
     try{
 		while(listening) {
 			System.out.println("Escuchando peticiones...");
 			socketCliente = socketServidor.accept();
 			System.out.println("Se acepto a un cliente");
-      System.out.println("Se acepto a un cliente");
-		  //-----------------------BAAAAAAD--------------------------
-		  //Para enviar mensajes a cliente
-		  PrintWriter out = new PrintWriter(socketCliente.getOutputStream(), true);
-		  //Para recibir mensajes del cliente
-		  BufferedReader in = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
-		  
-				   entrada = in.readLine();
-								System.out.println("Llego desde el cliente el mensaje: " + entrada);
-								out.println("mensaje: " + entrada + " recibido.");
-		  
-		  //------------------------WRONG--------------------------
-		  
-		  System.out.println("Cliente " + socketCliente.getInetAddress()+ " se desconecto.");
-				out.close();
-				in.close();
-		  socketCliente.close();
+			
+			
+
+			//Para enviar mensajes a cliente
+			PrintWriter out = new PrintWriter(socketCliente.getOutputStream(), true);
+			//Para recibir mensajes del cliente
+			BufferedReader in = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+			
+			
+			
+			while(true){  
+				entrada = in.readLine();
+				if(entrada.equals("salir")) {
+					break;
+				}
+			
+				System.out.println("Llego desde el cliente el mensaje: " + entrada);
+				out.println("mensaje: " + entrada + " recibido.");
+			}
+			  
+			//Se termina la conexion
+		  	//System.out.println("Cliente " + socketCliente.getInetAddress()+ " se desconecto.");
+			out.println("adios!");
+			out.close();
+			in.close();
+			socketCliente.close();
 		}
     } catch(IOException ioe) {
 		System.out.println("Ha ocurrido un error al escuchar al cliente");
 		ioe.printStackTrace();
     }
-    
-  }
+    int flag=2;
+}
 }
